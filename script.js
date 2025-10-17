8bb5154ab5114a5e01bf5ff1aa1620b0a01480f9
Aefrly <fraley.allison@ic.edu>
2025-10-17 18:14:39 -0500

    I was having a bit of trouble with creating my arrays but I figured things out after some time and now I have the arrays printing out.

--- script.js
+++ script.js
@@ -51,21 +51,25 @@ for (let i = 0; i < friendFavorites.length; i++) {
 }
 
 // 7. Create a new array longFoodNames for foods with names longer than 6 characters.
+let longFoodNames = [];
 for (let i = 0; i < friendFavorites.length; i++) {
   if (friendFavorites[i].length > 6) {
-    let longFoodNames = friendFavorites[i];
+    longFoodNames.push = friendFavorites[i];
   }
 }
 
 // 8. Create another array shortFoodNames for foods 6 characters or shorter.
+let shortFoodNames = [];
 for (let i = 0; i < friendFavorites.length; i++) {
   if (friendFavorites[i].length <= 6) {
-    let shortFoodNames = friendFavorites[i];
+    shortFoodNames.push = friendFavorites[i];
   }
 }
 
 // 9. Print both arrays and compare:
 // "There are more long-named foods." OR "There are more short-named foods."
+console.log(shortFoodNames);
+console.log(longFoodNames);
 
 
 // 10. STRETCH: Find the longest food name and print:
